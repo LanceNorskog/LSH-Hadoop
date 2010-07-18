74cd70ddcaaeb6a7cf88d7f2f2318b602ab4a0c9
U-norskhaus\lance <lance@norskhaus.(none)>
2010-07-17 20:31:33 -0700

    add counter

---
+++ project/src/hadoop/lsh/hadoop/CountReducer.java
@@ -0,0 +1,35 @@
+package lsh.hadoop;
+
+import java.io.IOException;
+import lsh.core.Point;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/*
+ * Receive one corner with a set of points.
+ * Key is the string printout of the Hash code (int,int,int,...int)
+ */
+
+/*
+ * Output format of "1,2,3 id,double...double|..."
+ */
+
+public class CountReducer extends
+		Reducer<Text, Text, Text, Text> {
+	
+	@Override
+	public void reduce(Text key, Iterable<Text> values, Context context)
+	throws IOException, InterruptedException {
+		StringBuilder sb = new StringBuilder();
+		int count = 0;
+
+		for(Text value: values) {
+			count++;
+		}
+		String value = Integer.toString(count);
+		context.write(new Text(key), new Text(value));
+	}
+
+}
+
