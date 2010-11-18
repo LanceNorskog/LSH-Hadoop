e30ecd282992e2db2b486607e8b480d9b2eea318
U-norskhaus\lance <lance@norskhaus.(none)>
2010-11-17 23:28:10 -0800

    remove junk that should not have been added

--- scripts/count.sh
+++
@@ -1,5 +0,0 @@
-
-
-export CLASSPATH="/cygwin/home/lance/github/LSH-Hadoop/project/bin/"
-export CLASSPATH="$CP;/cygwin/home/lance/github/LSH-Hadoop/project/bin/;/cygwin/home/lance/github/LSH-Hadoop/mahout/bin/"
-java lsh.hadoop.CountCorners 
