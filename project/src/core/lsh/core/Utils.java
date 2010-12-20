65c8ff48d9e8eb0ebed274adc83e8c9b1f63b309
U-norskhaus\lance <lance@norskhaus.(none)>
2010-12-19 21:02:27 -0800

    Add 'getDimensions' from Lookup data structure.

--- project/src/core/lsh/core/Utils.java
+++ project/src/core/lsh/core/Utils.java
@@ -16,10 +16,11 @@ public class Utils {
 
 	// very important - assumes multiple points per corner
 	// id2corner means point.id to corner
-	static public void load_corner_points_format(Reader r, String payload1, Lookup l1, String payload2, Lookup l2) throws IOException {
+	static public int load_corner_points_format(Reader r, String payload1, Lookup l1, String payload2, Lookup l2) throws IOException {
 		BufferedReader lnr = new BufferedReader(r);
 		String line;
 		int lines = 0;
+		int dimensions = -1;
 //		System.err.println("Loading corners... ");
 		while (null != (line = lnr.readLine())) {
 			String parts[] = line.split("[ \t]");
@@ -34,6 +35,7 @@ public class Utils {
 				l2.corners.add(corner);
 			for(int i = 0; i < pipes.length; i++) {
 				Point p = Point.newPoint(pipes[i]);
+				dimensions = p.values.length;
 				// I cannot believe it. String returns false for a null. 
 				if (null == payload1 || p.payload.equals(payload1)) {
 					addPair(l1.ids, l1.points, l1.corners, l1.id2point, l1.id2corner, l1.corner2ids, l1.corner2points, l1.point2corners, corner, p);
@@ -46,19 +48,22 @@ public class Utils {
 //			if (lines % 1000 == 0) {
 //				System.err.println("\t" + lines);
 //			}
-		}		
+		}	
+		return dimensions;
 	}
 
 	// very important - assumes multiple points per corner
 	// id2corner means point.id to corner
-	static public void load_point_corners_format(Reader r, String payload1, Lookup l1, String payload2, Lookup l2) throws IOException {
+	static public int load_point_corners_format(Reader r, String payload1, Lookup l1, String payload2, Lookup l2) throws IOException {
 		BufferedReader lnr = new BufferedReader(r);
+		int dimensions = -1;
 
 		String line;
 		while (null != (line = lnr.readLine())) {
 			String parts[] = line.split("[ \t]");
 			String[] pipes = parts[1].split("\\|");
 			Point point = Point.newPoint(parts[0]);
+			dimensions = point.values.length;
 			if (!payload1.equals(point.payload)) {
 				addPoint(l1.ids, l1.points, l1.id2point, point);
 				for(int i = 0; i < pipes.length; i++) {
@@ -72,25 +77,29 @@ public class Utils {
 					addPair(l2.ids, l2.points, l2.corners, l2.id2point, l2.id2corner, l2.corner2ids, l2.corner2points, l2.point2corners, corner, point);
 				}
 			}
-		}		
+		}
+		return dimensions;
 	}
 
 	// read point file, or point -> corner file
-	static public void load_point(Reader r, Set<Point> points, Set<String> ids,
+	static public int load_point(Reader r, Set<Point> points, Set<String> ids,
 			Map<String, Point> id2point, String payload) throws IOException {
 		BufferedReader lnr = new BufferedReader(r);
+		int dimensions = -1;
 
 		String line;
 		while (null != (line = lnr.readLine())) {
 			String[] parts = line.split("[ \t]");
 			Point point = Point.newPoint(parts[0]);
+			dimensions = point.values.length;
 			if (null != payload ) { 
 				if (null == point.payload || !payload.equals(point.payload)) {
 					continue;
 				}
 			}
 			addPoint(ids, points, id2point, point);
-		}		
+		}	
+		return dimensions;
 	}
 
 	static public void addPair(Set<String> ids, Set<Point> points, Set<Corner> corners, 
@@ -144,21 +153,24 @@ public class Utils {
 	
 	
 	// read corners, or corner part of a corner -> points file
-	public static void load_corner(Reader r, Set<Corner> corners,
+	public static int load_corner(Reader r, Set<Corner> corners,
 			Set<String> ids, Map<String, Corner> id2corner, String payload) throws IOException {
+	    int dimensions = -1;
 		BufferedReader lnr = new BufferedReader(r);
 
 		String line;
 		while (null != (line = lnr.readLine())) {
 			String[] parts = line.split("[ \t]");
 			Corner corner = Corner.newCorner(parts[0], parts.length > 1 ? parts[1] : null);
+			dimensions = corner.hashes.length;
 			if (null != payload ) { 
 				if (null == corner.payload || !payload.equals(corner.payload)) {
 					continue;
 				}
 			}
 			addCorner(ids, corners, id2corner, corner);
-		}		
+		}	
+		return dimensions;
 	}
 
 	private static void addCorner(Set<String> ids, Set<Corner> corners,
