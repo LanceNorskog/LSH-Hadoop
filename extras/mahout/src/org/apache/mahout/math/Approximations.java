33c26c2dcf666b722aaebf8cac46ac046b9d029c
U-norskhaus\lance <lance@norskhaus.(none)>
2011-04-09 22:32:15 -0700

    Bulletproof Approximations.

--- extras/mahout/src/org/apache/mahout/math/Approximations.java
+++ extras/mahout/src/org/apache/mahout/math/Approximations.java
@@ -23,24 +23,38 @@ package org.apache.mahout.math;
  * 
  * http://martin.ankerl.com/2007/02/11/optimized-exponential-functions-for-java/
  * http://martin.ankerl.com/2007/10/04/optimized-pow-approximation-for-java-and-c-c/
+ * 
+ * Caveats:
+ * log(n) is invalid where n < 1. For n > 1, error starts at 5% and rapidly drops.
+ * exp(n) is valid from 0.1 < n < 800. 800 is the limit for exp(n).
+ * pow(n, exp) is valid for exp < 1
+ * 
+ * Throw exception if invalid value given. The alternative is to switch to the accurate version.
+ * But the point of using Approximations is speed- invisibly switching to the slow versions is not what I would want.
  */
 
 public class Approximations {
-
-  public static double pow(double value, double exponent) {
+  
+  public static double pow(double value, double exponent) throws Exception {
+    if (exponent > 2.0)
+      throw new Exception("Approximations.pow(n) only valid where n < 1");
     final int x = (int) (Double.doubleToLongBits(value) >> 32);
     final int y = (int) (exponent * (x - 1072632447) + 1072632447);
     return Double.longBitsToDouble(((long) y) << 32);
   }
   
-  public static double exp(double value) {
+  public static double exp(double value) throws Exception {
+    if (value < 0.1)
+      throw new Exception("Approximations.exp(n) only valid where n > 1");
     final long tmp = (long) (1512775 * value + (1072693248 - 60801));
     return Double.longBitsToDouble(tmp << 32);
   }
   
-  public static double log(double val) {
+  public static double log(double val) throws Exception {
+    if (val > 2)
+      throw new Exception("Approximations.log(n) only valid where n < 1");
     final double x = (Double.doubleToLongBits(val) >> 32);
     return (x - 1072632447) / 1512775;
-}
+  }
   
 }
